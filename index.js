944b2c438304d60e9cfd58a3abc22a1fa4658f08
tattii <yuta.tatti@gmail.com>
2015-03-08 21:08:57 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+var http = require('http');
+
+app.set('port', (process.env.PORT || 5001));
+app.use(express.static(__dirname + '/public'));
+app.use('/components', express.static(__dirname + '/components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+
+app.get("/blclnt", function(req, res) {
+	var url = "http://blsetup.city.kyoto.jp/cgi-bin/blclnt.cgi?=" + req.param("line");
+	http.get(url, function(blclnt) {
+		blclnt.on('data', function (chunk) {
+			res.setHeader("Content-Type", "text/plain");
+			res.send(chunk);
+		});
+
+	}).on('error', function(e) {
+		res.send("error: " + e.message);
+	});
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
